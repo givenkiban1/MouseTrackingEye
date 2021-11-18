7669a2a81c36b12508eefb6d51fdb90cbaaa9ce2
Given Kibanza <gkibanzait@Gmail.com>
2021-11-18 16:34:01 +0200

    depth and angles implemented

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ function resetPosition(event){
     document.getElementById("eye").style.marginTop = "150" + "px";
 }
 
+var angles = [];
+var anglesY = [];
+
+for (var i=-50; i <50 ; i++){
+    angles.push(i);
+    anglesY.push(-i);
+}
+
+
+
+// console.log(angles);
+
 function calculatePosition(clientX, clientY){
     // this function is called when the mousepad moves over the bg-image div
     // default margin from top/left = 8px
@@ -28,29 +40,56 @@ function calculatePosition(clientX, clientY){
     // the code below checks that the mouse's coordinates
     // are within the boundary box
     // if exceeding, the coordinate is set to the limit
-    if (mLeft <157){
-        mLeft = 157;
+    if (mLeft <200){
+        mLeft = 200;
     }
     else
-    if (mLeft > 360){
-        mLeft = 360;
+    if (mLeft > 310){
+        mLeft = 310;
     }
 
-    if (mTop < 196){
-        mTop = 196;
+    if (mTop < 230){
+        mTop = 230;
     }
     else
-    if (mTop > 420){
-        mTop = 420;
+    if (mTop > 380){
+        mTop = 380;
     }
 
     // assigning the coordinates of the eye to follow
     // offset is added to make sure that the eyeball is directly
     // under the mouse.
+    // console.log(
+    //     (mLeft- 124) + " , " +
+    //     (mTop-150)
+    // );
+
+    // right-edge: rotateY(-50deg)
+    // left edge: rotateY(50deg)
+    // Y angle range: -1 * (-50 to 50)_
+
+    var xRange = 310 - 200;
+    var yRange = 380 - 230;
+    var yAngleRange = 100;
+
+    var xyAngleRate = yAngleRange / xRange;
+    var yyAngleRate = yAngleRange / yRange;
+
     document.getElementById("eye").style.marginLeft = mLeft- 124 + "px";
+    // console.log(Math.ceil((mLeft- 124) * xyAngleRate)-70);
+    // console.log(Math.ceil((mTop- 150) * yyAngleRate)-54);
+    // console.log( angles[Math.ceil((mLeft- 124) * xyAngleRate)] );
+    var angleIndex = Math.ceil((mLeft- 124) * xyAngleRate)-70;
+    // document.getElementById("eye").style.transform = "rotateY(" + angles[angleIndex] + "deg)";
+    
+    var angleIndey = Math.ceil((mTop- 150) * yyAngleRate)-54;
+    // document.getElementById("eye").style.transform = "rotateX(" + angles[angleIndey] + "deg)";
 
     document.getElementById("eye").style.marginTop = mTop-150 + "px";
-
+    document.getElementById("eye").style.transform = "rotateY(" + angles[angleIndex] + "deg) "
+    // ;
+     + 
+    "rotateX(" + (-angles[angleIndey]) + "deg)";
 
 }
 
